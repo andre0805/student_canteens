a67826dd4863c50731d236a9009f5aa76f625bbc
Andre Flego <ade.flego@gmail.com>
2023-04-28 22:31:04 +0200

    configure auth view

---
+++ lib/views/auth/AuthView.dart
@@ -0,0 +1,141 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class AuthView extends StatelessWidget {
+  String email = "";
+  String password = "";
+
+  AuthView({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                const Spacer(),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                ),
+                TextField(
+                  onChanged: (value) {
+                    // TODO: Add email validation
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade900),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    // TODO: Add password validation
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Lozinka",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade900),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(
+                  onPressed: () => {
+                    // TODO: Add login logic
+                  },
+                  child: Text("Prijavi se"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[900],
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Expanded(
+                      child: Divider(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      " ili ",
+                      style: TextStyle(color: Colors.grey[800]),
+                    ),
+                    const Expanded(
+                      child: Divider(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                SignInButton(
+                  Buttons.Google,
+                  onPressed: () => {},
+                  text: "Prijava s Google-om",
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                SignInButton(
+                  Buttons.Facebook,
+                  onPressed: () => {},
+                  text: "Prijava s Facebook-om",
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Nemaš račun?",
+                    ),
+                    TextButton(
+                      onPressed: () => {
+                        // TODO: Add register logic
+                      },
+                      child: const Text(
+                        "Registriraj se",
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
