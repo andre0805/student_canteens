8be31081ef4dcb1f02227bdec695cb074c3358a5
Andre Flego <ade.flego@gmail.com>
2023-05-01 15:00:08 +0200

    update title for MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Student Canteens',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
