c08796f8c4c5683cc21681b9547ffef19bfeee44
Andre Flego <ade.flego@gmail.com>
2023-05-02 14:48:00 +0200

    change method signatures in auth service

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -1,17 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  Future<UserCredential> signUp(String email, String password) async {
-    return await FirebaseAuth.instance
+  Future<void> signUp(String email, String password) async {
+    await FirebaseAuth.instance
         .createUserWithEmailAndPassword(email: email, password: password);
   }
 
-  Future<UserCredential> signIn(String email, String password) async {
-    return await FirebaseAuth.instance
+  Future<void> signIn(String email, String password) async {
+    await FirebaseAuth.instance
         .signInWithEmailAndPassword(email: email, password: password);
   }
 
-  void signOut() async {
+  Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
   }
 }
