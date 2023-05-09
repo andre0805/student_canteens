10069690cf92770e8ea483cf9bc182a62aa8eee7
Andre Flego <ade.flego@gmail.com>
2023-05-10 00:08:40 +0200

    add models for Canteen, City and County

--- lib/models/Canteen.dart
+++ lib/models/Canteen.dart
@@ -1,1 +1,25 @@
-class Canteen {}
+import 'dart:ffi';
+
+class Canteen {
+  final Long id;
+  final String name;
+  final String address;
+  final int cityId;
+  final String contact;
+  final String url;
+  final String imageUrl;
+  final String latitude;
+  final String longitude;
+
+  Canteen({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.cityId,
+    required this.contact,
+    required this.url,
+    required this.imageUrl,
+    required this.latitude,
+    required this.longitude,
+  });
+}
---
+++ lib/models/County.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+class County {
+  final Long id;
+  final String name;
+
+  County({
+    required this.id,
+    required this.name,
+  });
+}
