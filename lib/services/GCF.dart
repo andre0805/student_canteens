2b143094d7e45508876fa7e12e4c4b2ad36ac37e
Andre Flego <ade.flego@gmail.com>
2023-05-10 00:07:35 +0200

    add method for getting user data

--- lib/services/GCF.dart
+++ lib/services/GCF.dart
@@ -6,11 +6,28 @@ class GCF {
   static const String BASE_URL =
       'https://us-central1-student-canteens.cloudfunctions.net';
   static const String GET_CANTEENS = '/getCanteens';
+  static const String GET_USER = '/getUser';
   static const String CREATE_USER = '/createUser';
 
   static const GCF sharedInstance = GCF._();
 
   const GCF._();
+
+  Future<SCUser?> getUser(String email) async {
+    http.Response response = await http.get(
+      Uri.parse(BASE_URL + GET_USER + '?email=' + email),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      return SCUser.fromJson(json);
+    } else {
+      return null;
+    }
+  }
 
   Future<http.Response> createUser(SCUser user) async {
     return http.post(
