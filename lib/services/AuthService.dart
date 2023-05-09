f703f4dad3da2f4924b62386d368d7ed0a9dee04
Andre Flego <ade.flego@gmail.com>
2023-05-10 00:08:19 +0200

    refactor auth methods

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -2,9 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:student_canteens/models/SCUser.dart';
 import 'package:student_canteens/services/GCF.dart';
+import 'package:student_canteens/services/SessionManager.dart';
 
 class AuthService {
   GCF gcf = GCF.sharedInstance;
+  SessionManager sessionManager = SessionManager.sharedInstance;
 
   Future<void> signUp(SCUser user, String password) async {
     UserCredential userCredential = await FirebaseAuth.instance
@@ -21,8 +23,20 @@ class AuthService {
   }
 
   Future<void> signIn(String email, String password) async {
-    await FirebaseAuth.instance
+    SCUser? user = await gcf.getUser(email);
+
+    if (user == null) {
+      throw Exception("User not found");
+    }
+
+    UserCredential userCredential = await FirebaseAuth.instance
         .signInWithEmailAndPassword(email: email, password: password);
+
+    if (userCredential.user == null) {
+      throw Exception("User not found");
+    }
+
+    sessionManager.signIn(user);
   }
 
   Future<void> signInWithGoogle() async {
@@ -37,19 +51,32 @@ class AuthService {
     UserCredential userCredential =
         await FirebaseAuth.instance.signInWithCredential(credential);
 
-    User user = userCredential.user!;
+    User? user = userCredential.user;
 
-    SCUser newUser = SCUser(
-      id: userCredential.user!.uid,
-      name: user.displayName ?? "Unknown",
-      surname: "",
-      email: googleUser.email,
-    );
+    if (user == null) {
+      throw Exception("User not found");
+    }
 
-    await gcf.createUser(newUser);
+    SCUser? scUser = await gcf.getUser(user.email!);
+
+    if (scUser != null) {
+      sessionManager.signIn(scUser);
+      return;
+    } else {
+      SCUser newUser = SCUser(
+        id: userCredential.user!.uid,
+        name: user.displayName ?? "Unknown",
+        surname: "",
+        email: googleUser.email,
+      );
+
+      await gcf.createUser(newUser);
+      sessionManager.signIn(newUser);
+    }
   }
 
   Future<void> signOut() async {
+    sessionManager.signOut();
     await FirebaseAuth.instance.signOut();
   }
 
