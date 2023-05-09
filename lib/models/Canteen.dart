469888b3c390aacaa3a78f3df12d347b8c4fcbcb
Andre Flego <ade.flego@gmail.com>
2023-05-10 01:49:08 +0200

    refactor Canteen model

--- lib/models/Canteen.dart
+++ lib/models/Canteen.dart
@@ -1,13 +1,13 @@
 import 'dart:ffi';
 
 class Canteen {
-  final Long id;
+  final int id;
   final String name;
   final String address;
   final int cityId;
-  final String contact;
-  final String url;
-  final String imageUrl;
+  final String? contact;
+  final String? url;
+  final String? imageUrl;
   final String latitude;
   final String longitude;
 
@@ -22,4 +22,18 @@ class Canteen {
     required this.latitude,
     required this.longitude,
   });
+
+  factory Canteen.fromJson(Map<String, dynamic> json) {
+    return Canteen(
+      id: json['id'],
+      name: json['name'],
+      address: json['address'],
+      cityId: json['city_id'],
+      contact: json['contact'],
+      url: json['url'],
+      imageUrl: json['cover_picture'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+    );
+  }
 }
