07429fc9e9356867f2cf6dfc578b1c67ff22ec7e
Andre Flego <ade.flego@gmail.com>
2023-05-10 22:44:28 +0200

    configure themes

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,23 @@ class MyApp extends StatelessWidget {
       title: 'Student Canteens',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+        // primaryColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey[900],
+          foregroundColor: Colors.white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey[900],
+            disabledBackgroundColor: Colors.grey[400],
+            foregroundColor: Colors.white,
+          ),
+        ),
+        progressIndicatorTheme: const ProgressIndicatorThemeData(
+          color: Colors.blue,
+        ),
       ),
       home: Scaffold(
         body: StreamBuilder<User?>(
