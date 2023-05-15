3a17ed839b569d38b4e412870de714ca8a947ff2
Andre Flego <ade.flego@gmail.com>
2023-05-15 19:44:41 +0200

    show canteen details

--- lib/views/canteens/CanteenView.dart
+++ lib/views/canteens/CanteenView.dart
@@ -1,7 +1,12 @@
+import 'dart:collection';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:student_canteens/models/Canteen.dart';
+import 'package:student_canteens/models/WrokSchedule.dart';
 import 'package:student_canteens/services/GCF.dart';
 import 'package:student_canteens/services/StorageService.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CanteenView extends StatefulWidget {
   final Canteen canteen;
@@ -20,13 +25,37 @@ class _CanteenViewState extends State<CanteenView> {
   StorageService storageService = StorageService();
   GCF gcf = GCF.sharedInstance;
 
+  Set<WorkSchedule> workSchedules = SplayTreeSet((key1, key2) {
+    return key1.compareTo(key2);
+  });
+
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      isLoading = true;
+    });
+
+    Future.wait([
+      getWorkschedule(),
+    ]).then((value) {
+      setState(() {
+        workSchedules = value[0];
+        isLoading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: RefreshIndicator(
         onRefresh: refreshWidget,
-        edgeOffset: 180,
+        edgeOffset: 120,
         child: CustomScrollView(
           slivers: [
             // app bar
@@ -38,13 +67,213 @@ class _CanteenViewState extends State<CanteenView> {
                 },
                 icon: const Icon(Icons.arrow_back),
               ),
-              title: Text(
-                canteen.name,
-                style: const TextStyle(
+              title: const Text(
+                "",
+                style: TextStyle(
                   color: Colors.white,
                 ),
               ),
-              actions: [],
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.star_border),
+                ),
+              ],
+            ),
+
+            // loading indicator
+            SliverVisibility(
+              visible: isLoading,
+              sliver: const SliverFillRemaining(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ),
+
+            // google map
+            SliverVisibility(
+              visible: !isLoading,
+              sliver: SliverToBoxAdapter(
+                child: Container(
+                  height: 200,
+                  color: Colors.green[400],
+                  child: const Center(
+                    child: Text("Google map"),
+                  ),
+                ),
+              ),
+            ),
+
+            SliverVisibility(
+              visible: !isLoading,
+              sliver: SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // canteen name
+                      Text(
+                        canteen.name,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      const SizedBox(height: 8),
+
+                      // canteen address
+                      Text(
+                        canteen.address,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+
+                      const SizedBox(height: 24),
+
+                      // work schedule
+                      Visibility(
+                        visible: workSchedules.isNotEmpty,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Radno vrijeme",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            ListView.builder(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 8,
+                                horizontal: 0,
+                              ),
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: workSchedules.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 2,
+                                    horizontal: 2,
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 40,
+                                        child: Text(
+                                          workSchedules
+                                              .elementAt(index)
+                                              .getDayOfWeekAbbrString(),
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(width: 16),
+                                      Text(
+                                        workSchedules
+                                            .elementAt(index)
+                                            .getTimesString(),
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                            const SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+
+                      // contact
+                      Visibility(
+                        visible: canteen.contact != null,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Kontakt",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              canteen.contact.toString(),
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 24),
+                          ],
+                        ),
+                      ),
+
+                      // web
+                      Visibility(
+                        visible: canteen.url != null,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Web",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            InkWell(
+                              onTap: openCanteenWeb,
+                              child: const Text(
+                                "Otvori web",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.blue,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: Colors.blue,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(height: 24),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            // work schedule
+            SliverVisibility(
+              visible: !isLoading,
+              sliver: SliverToBoxAdapter(),
+            ),
+
+            // contact
+            SliverVisibility(
+              visible: !isLoading,
+              sliver: SliverToBoxAdapter(),
+            ),
+
+            // web
+            SliverVisibility(
+              visible: !isLoading,
+              sliver: SliverToBoxAdapter(),
             ),
           ],
         ),
@@ -53,10 +282,24 @@ class _CanteenViewState extends State<CanteenView> {
   }
 
   Future<void> refreshWidget() async {
-    return getCanteen();
+    Future.wait([
+      getWorkschedule(),
+    ]).then((value) {
+      setState(() {
+        workSchedules = value[0];
+      });
+    });
   }
 
-  Future<void> getCanteen() async {
-    gcf.getCanteens();
+  Future<Set<WorkSchedule>> getWorkschedule() async {
+    return gcf.getWorkSchedule(canteen.id);
+  }
+
+  void openCanteenWeb() async {
+    if (await canLaunchUrl(Uri.parse(canteen.url.toString()))) {
+      await launchUrl(Uri.parse(canteen.url.toString()));
+    } else {
+      print('Could not launch ${canteen.url.toString()}');
+    }
   }
 }
