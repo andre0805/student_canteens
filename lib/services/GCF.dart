93453e44a852fe3da674048bedc7629fa5d20e4e
Andre Flego <ade.flego@gmail.com>
2023-05-15 19:44:24 +0200

    add method for fetching canteen by id and its work schedule

--- lib/services/GCF.dart
+++ lib/services/GCF.dart
@@ -3,12 +3,16 @@ import 'package:student_canteens/models/SCUser.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:student_canteens/models/WrokSchedule.dart';
+
 class GCF {
   static const String BASE_URL =
       'https://us-central1-student-canteens.cloudfunctions.net';
   static const String GET_CANTEENS = '/getCanteens';
+  static const String GET_CANTEEN = '/getCanteen';
   static const String GET_USER = '/getUser';
   static const String CREATE_USER = '/createUser';
+  static const String GET_WORK_SCHEDULE = '/getWorkSchedule';
 
   static const GCF sharedInstance = GCF._();
 
@@ -60,6 +64,39 @@ class GCF {
       return json.map((e) => Canteen.fromJson(e)).toList();
     } else {
       return [];
+    }
+  }
+
+  Future<Canteen?> getCanteen(int canteenId) async {
+    http.Response response = await http.get(
+      Uri.parse(BASE_URL + GET_CANTEEN + '?canteenId=' + canteenId.toString()),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      return Canteen.fromJson(json);
+    } else {
+      return null;
+    }
+  }
+
+  Future<Set<WorkSchedule>> getWorkSchedule(int canteenId) async {
+    http.Response response = await http.get(
+      Uri.parse(
+          BASE_URL + GET_WORK_SCHEDULE + '?canteenId=' + canteenId.toString()),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> json = jsonDecode(response.body);
+      return json.map((e) => WorkSchedule.fromJson(e)).toSet();
+    } else {
+      return {};
     }
   }
 }
