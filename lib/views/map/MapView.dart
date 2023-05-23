1e296037d103713c0bbe3a9a3d57f495f69df936
Andre Flego <ade.flego@gmail.com>
2023-05-23 18:00:55 +0200

    add markers for displaying canteens on map

--- lib/views/map/MapView.dart
+++ lib/views/map/MapView.dart
@@ -2,6 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:student_canteens/services/GCF.dart';
+import 'package:student_canteens/views/canteen/CanteenView.dart';
 
 class MapView extends StatefulWidget {
   @override
@@ -9,12 +11,47 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  final GCF gcf = GCF.sharedInstance;
+
   GoogleMapController? mapController;
   LatLng center = const LatLng(44.891630, 16.476338);
+
+  Set<Marker> markers = {};
 
   @override
   void initState() {
     super.initState();
+
+    Future.wait([
+      gcf.getCanteens(),
+    ]).then((values) {
+      updateWidget(() {
+        markers = values[0].map((canteen) {
+          return Marker(
+            markerId: MarkerId(canteen.id.toString()),
+            position: LatLng(
+              double.parse(canteen.latitude),
+              double.parse(canteen.longitude),
+            ),
+            infoWindow: InfoWindow(
+              title: canteen.name,
+              snippet: canteen.address,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CanteenView(
+                      canteen: canteen,
+                      parentRefreshWidget: () {},
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }).toSet();
+      });
+    });
   }
 
   @override
@@ -50,7 +87,7 @@ class _MapViewState extends State<MapView> {
             onMapCreated: (controller) {
               mapController = controller;
             },
-            markers: {},
+            markers: markers,
             gestureRecognizers: {
               Factory<OneSequenceGestureRecognizer>(
                   () => EagerGestureRecognizer())
@@ -59,5 +96,9 @@ class _MapViewState extends State<MapView> {
         ),
       ],
     );
+  }
+
+  void updateWidget(VoidCallback callback) {
+    if (mounted) setState(callback);
   }
 }
