6316827d1e2812c4f9a59d205667c7820da23311
Andre Flego <ade.flego@gmail.com>
2023-05-28 05:41:58 +0200

    add ios notifications

--- lib/services/NotificationService.dart
+++ lib/services/NotificationService.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:student_canteens/utils/Constants.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 class NotificationService {
   static final NotificationService sharedInstance = NotificationService._();
@@ -11,20 +16,49 @@ class NotificationService {
     int minute,
     Function() callback,
   ) async {
-    await AndroidAlarmManager.periodic(
-      const Duration(days: 1),
-      id,
-      callback,
-      startAt: DateTime(
-        DateTime.now().year,
-        DateTime.now().month,
-        DateTime.now().day,
-        hour,
-        minute,
-      ),
-      wakeup: true,
-      rescheduleOnReboot: true,
+    var now = tz.TZDateTime.now(tz.local);
+    var scheduledDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      hour,
+      minute,
     );
+
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
+    }
+
+    if (Platform.isAndroid) {
+      await AndroidAlarmManager.periodic(
+        const Duration(days: 1),
+        id,
+        callback,
+        startAt: scheduledDate,
+        wakeup: true,
+        rescheduleOnReboot: true,
+        exact: true,
+      );
+    }
+
+    if (Platform.isIOS) {
+      const iOSPlatformChannelSpecifics = DarwinNotificationDetails();
+      const platformChannelSpecifics = NotificationDetails(
+        iOS: iOSPlatformChannelSpecifics,
+      );
+
+      await FlutterLocalNotificationsPlugin().zonedSchedule(
+        Constants.notificationId,
+        'Hej! Vrijeme je za ručak! 🍽',
+        'Provjeri stanje redova u menzama i odaberi najbolju opciju za sebe!',
+        scheduledDate,
+        platformChannelSpecifics,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time,
+      );
+    }
   }
 
   Future<void> cancelNotifications(int id) async {
