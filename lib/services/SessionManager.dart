daf3476ae60c1043f7d8b082a9f392abdf350517
Andre Flego <ade.flego@gmail.com>
2023-05-28 03:49:19 +0200

    configure app when user signs in or out

--- lib/services/SessionManager.dart
+++ lib/services/SessionManager.dart
@@ -1,17 +1,47 @@
+import 'dart:io';
+
 import 'package:student_canteens/models/SCUser.dart';
+import 'package:student_canteens/services/NotificationService.dart';
+import 'package:student_canteens/services/StorageService.dart';
+import 'package:student_canteens/utils/utils.dart';
 
 class SessionManager {
+  static final StorageService storageService = StorageService.sharedInstance;
+  static final NotificationService notificationService =
+      NotificationService.sharedInstance;
+
   static final SessionManager sharedInstance = SessionManager._();
 
   SessionManager._();
 
   SCUser? currentUser;
 
-  void signIn(SCUser user) {
+  Future<void> signIn(SCUser user) async {
     currentUser = user;
+
+    if (user.city != null) {
+      await storageService.saveString("selectedCity", user.city!);
+      await storageService.saveString("userCity", user.city!);
+    }
+
+    if (user.lunchTime != null && Platform.isAndroid) {
+      await notificationService.scheduleNotifications(
+        0,
+        user.lunchTime!.hour,
+        user.lunchTime!.minute,
+        Utils.notificationTask,
+      );
+      print(
+          "Scheduled notifications for lunch at ${user.lunchTime!.hour}:${user.lunchTime!.minute}");
+    }
   }
 
-  void signOut() {
+  Future<void> signOut() async {
     currentUser = null;
+    await storageService.removeKey('selectedCity');
+    await storageService.removeKey('selectedDrawerItemIndex');
+    await storageService.removeKey('selectedSortCriteria');
+    await storageService.removeKey('userCity');
+    await notificationService.cancelNotifications(0);
   }
 }
