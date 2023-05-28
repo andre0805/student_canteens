b4c7a3fb5503c5095e5875843367a289452467ae
Andre Flego <ade.flego@gmail.com>
2023-05-28 03:49:28 +0200

    define method for notificaiton task

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:student_canteens/models/Canteen.dart';
+import 'package:student_canteens/models/QueueLength.dart';
+import 'package:student_canteens/services/GCF.dart';
+import 'package:student_canteens/services/StorageService.dart';
 
 class Utils {
   static void showLoadingDialog(BuildContext context) {
@@ -149,5 +156,65 @@ class Utils {
 
   static void hideCurrentSnackBar(BuildContext context) {
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
+  }
+
+  static void notificationTask() async {
+    // Fetch data
+    final canteens = await GCF.sharedInstance.getCanteens();
+    final city = await StorageService.sharedInstance.getString("userCity");
+
+    String notificationTitle;
+    String notificationBody = "";
+
+    if (city == null) return;
+
+    // get canteens in city
+    final List<Canteen> canteensInCity =
+        canteens.where((canteen) => canteen.city == city).toList();
+    final List<Canteen> shortestQueueCanteens = canteensInCity
+        .where((element) => element.queueLength != QueueLength.UNKNOWN)
+        .toList();
+    shortestQueueCanteens.sort(
+        (a, b) => QueueLengthExtension.compare(a.queueLength, b.queueLength));
+
+    if (shortestQueueCanteens.isEmpty) {
+      notificationTitle = "Hej! Vrijeme je za ručak! 🍽";
+      notificationBody =
+          "Nažalost, trenutno nemamo dostupnih informacija o redovima u menzama, ali svakako navrati kasnije i provjeri stanje u menzama.";
+    } else if (shortestQueueCanteens.length == 1) {
+      notificationTitle = "Hej! Vrijeme je za ručak! 🍽";
+      notificationBody =
+          "Trenutno imamo samo informaciju o stanju reda u menzi ${shortestQueueCanteens[0].name}: ${QueueLengthExtension.getLegendDescription(shortestQueueCanteens[0].queueLength)}.";
+    } else {
+      notificationTitle =
+          "Hej! Vrijeme je za ručak! 🍽 Pogledaj dostupne opcije.";
+      notificationBody += "Najkraći redovi su u menzama:";
+      for (Canteen c in shortestQueueCanteens.sublist(
+          0, min(3, shortestQueueCanteens.length))) {
+        notificationBody +=
+            " ${c.name} - ${QueueLengthExtension.getLegendDescription(c.queueLength)}";
+        if (c != shortestQueueCanteens.last) {
+          notificationBody += ", ";
+        }
+      }
+    }
+
+    // Show notification with the data
+    const androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channelId',
+      'channelName',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+      styleInformation: BigTextStyleInformation(''),
+    );
+    const platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await FlutterLocalNotificationsPlugin().show(
+      0,
+      notificationTitle,
+      notificationBody,
+      platformChannelSpecifics,
+    );
   }
 }
